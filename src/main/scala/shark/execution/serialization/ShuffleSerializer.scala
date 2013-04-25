1a2d5b6821bbdf0861984d6887065f91dd0b2a06
Patrick Wendell <pwendell@gmail.com>
2013-04-25 15:13:42 -0700

    Bug fix and test for shuffle serializer

--- src/main/scala/shark/execution/serialization/ShuffleSerializer.scala
+++ src/main/scala/shark/execution/serialization/ShuffleSerializer.scala
@@ -66,7 +66,7 @@ class ShuffleSerializationStream(stream: OutputStream) extends SerializationStre
 
   override def flush(): Unit = stream.flush()
 
-  override def close(): Unit = {}
+  override def close(): Unit = stream.close()
 
   def writeInt(value: Int) {
     stream.write(value >> 24)
@@ -79,29 +79,27 @@ class ShuffleSerializationStream(stream: OutputStream) extends SerializationStre
 
 class ShuffleDeserializationStream(stream: InputStream) extends DeserializationStream {
 
-  //val keyBytes = new BytesWritable
-  //val reduceKey = new ReduceKey(keyBytes)
-  //val valueBytes = new BytesWritable
-
   override def readObject[T](): T = {
     val keyLen = readInt()
     if (keyLen < 0) {
       throw new java.io.EOFException
     }
     val valueLen = readInt()
-    // keyBytes.setSize(keyLen)
-    // stream.read(keyBytes.getBytes(), 0, keyLen)
-    // valueBytes.setSize(valueLen)
-    // stream.read(valueBytes.getBytes(), 0, valueLen)
-    // (new ReduceKey(keyBytes), valueBytes).asInstanceOf[T]
     val keyBytes = new BytesWritable(new Array[Byte](keyLen))
-    stream.read(keyBytes.getBytes(), 0, keyLen)
+    readFully(stream, keyBytes.getBytes, keyLen)
     val valueBytes = new BytesWritable(new Array[Byte](valueLen))
-    stream.read(valueBytes.getBytes(), 0, valueLen)
+    readFully(stream, valueBytes.getBytes, valueLen)
     (new ReduceKey(keyBytes), valueBytes).asInstanceOf[T]
   }
 
-  override def close() { }
+  def readFully(stream: InputStream, bytes: Array[Byte], length: Int) {
+    var read = 0
+    while (read < length) {
+      read += stream.read(bytes, read, length - read)
+    }
+  }
+
+  override def close() = stream.close()
 
   def readInt(): Int = {
     stream.read() << 24 | stream.read() << 16 | stream.read() << 8 | stream.read()
---
+++ src/test/scala/shark/SerializationSuite.scala
@@ -0,0 +1,70 @@
+package shark
+
+import execution.ReduceKey
+import org.scalatest.FunSuite
+import shark.execution.serialization.ShuffleSerializer
+import org.apache.hadoop.io.BytesWritable
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import org.scalatest.matchers.ShouldMatchers
+import com.ning.compress.lzf.{LZFInputStream, LZFOutputStream}
+
+class SerializationSuite extends FunSuite with ShouldMatchers {
+  test("Serializing and deserializing from a stream") {
+    val NUM_ITEMS = 5000
+    val KEY_SIZE = 1000
+    val VALUE_SIZE = 1000
+
+    val initialItems =
+      (1 to NUM_ITEMS).map {x =>
+        val rkBytes = (1 to KEY_SIZE).map(_.toByte).toArray
+        val valueBytes = (1 to VALUE_SIZE).map(_.toByte).toArray
+        val rk = new ReduceKey(new BytesWritable(rkBytes))
+        val value = new BytesWritable(valueBytes)
+        (rk, value)
+      }
+
+    val bos = new ByteArrayOutputStream()
+    val ser = new ShuffleSerializer()
+    val serStream = ser.newInstance().serializeStream(bos)
+    initialItems.map(serStream.writeObject(_))
+    val bis = new ByteArrayInputStream(bos.toByteArray)
+    val serInStream = ser.newInstance().deserializeStream(bis)
+
+    initialItems.map{ x =>
+      val output: (ReduceKey, BytesWritable) = serInStream.readObject()
+      output should equal (x)
+    }
+  }
+
+  test("Serializing and deserializing from a stream (with compression)") {
+    val NUM_ITEMS = 1000
+    val KEY_SIZE = 1000
+    val VALUE_SIZE = 1000
+
+    val initialItems =
+      (1 to NUM_ITEMS).map {x =>
+        val rkBytes = (1 to KEY_SIZE).map(_.toByte).toArray
+        val valueBytes = (1 to VALUE_SIZE).map(_.toByte).toArray
+        val rk = new ReduceKey(new BytesWritable(rkBytes))
+        val value = new BytesWritable(valueBytes)
+        (rk, value)
+      }
+
+    val bos = new ByteArrayOutputStream()
+    val cBos = new LZFOutputStream(bos)
+    val ser = new ShuffleSerializer()
+    val serStream = ser.newInstance().serializeStream(cBos)
+    initialItems.map(serStream.writeObject(_))
+    serStream.close()
+    val array = bos.toByteArray
+    val bis = new ByteArrayInputStream(array)
+    println("Total bytes serialized: " + array.length)
+    val cBis = new LZFInputStream(bis)
+    val serInStream = ser.newInstance().deserializeStream(cBis)
+
+    initialItems.map{ x =>
+      val output: (ReduceKey, BytesWritable) = serInStream.readObject()
+      output should equal (x)
+    }
+  }
+}
